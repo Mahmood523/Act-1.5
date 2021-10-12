c29f83db3163d1dae6e8c0eed5d56eff2489f38f
Mahmood523 <91906977+Mahmood523@users.noreply.github.com>
2021-10-12 10:26:24 +0200

    Add files via upload

---
+++ Mainb.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	Bateau b= new Bateau(1);
+	Coque c = new Coque(2);
+	Cabine ca = new Cabine(3);
+	Pont p = new Pont(4);
+	Mat m = new Mat(5);
+	Voile v = new Voile(6);
+	
+	
+		
+	System.out.println(b+" est constitué de:");
+	System.out.println(c);
+	System.out.println(ca);
+	System.out.println(p+""+m+""+v);
+			
+	} 
+	
+
+}
